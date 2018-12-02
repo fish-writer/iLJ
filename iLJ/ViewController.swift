2df96cb42623ddd3da87042c2b99a8e32f944b94
hfernan <hfernan@ncsu.edu>
2018-12-01 21:46:16 -0500

    Initial Commit

---
+++ iLJ/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iLJ
+//
+//  Created by Hanna Fernandez on 12/1/18.
+//  Copyright © 2018 fish-writer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
